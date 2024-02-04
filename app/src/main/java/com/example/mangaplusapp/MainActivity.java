93412c6d5e714c83039e830fe680c4e02c482ff2
rena2302 <khoivu2302@gmail.com>
2024-02-04 22:46:12 +0700

    Delete DrawerFragment Update PressOnBack, Dynamic Toolbar (demo), ImageSlider, HomePage, Slide Up ImageSlider (demo)

--- app/src/main/java/com/example/mangaplusapp/MainActivity.java
+++ app/src/main/java/com/example/mangaplusapp/MainActivity.java
@@ -5,15 +5,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuProvider;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -30,7 +28,6 @@ public class MainActivity extends AppCompatActivity{
     private ActivityMainBinding binding;
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
-    private FormLayout formLayout;
     private DrawerLayout drawerLayout;
     private FragmentManager fragmentManager;
 
